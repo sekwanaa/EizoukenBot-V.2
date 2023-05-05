2469120e3786db2091b180828fdbf06cfaac18ff
sekwanaa <sk123rulz@gmail.com>
2023-05-05 18:26:40 -0400

    moved add_theme and remove_theme to moderater use changed default member permission to manage guild

--- Commands/Moderator/add_theme.js
+++ Commands/Moderator/add_theme.js
@@ -4,12 +4,19 @@ const themesData = require("../../data/themesData")
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("remove_theme")
+    .setName("add_theme")
     .setDescription("Allows user to add/change a theme for a month")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .addStringOption(option => 
         option
         .setName("month")
         .setDescription("Choose a month to add/update a theme for")
+        .setRequired(true)
+    )
+    .addStringOption(option => 
+        option
+        .setName("theme")
+        .setDescription("Input a theme for the month")
         .setRequired(true)
     )
     .addIntegerOption(option => 
@@ -22,15 +29,16 @@ module.exports = {
     async execute(interaction) {
         const { channel, options } = interaction;
         const month = options.getString("month");
+        const theme = options.getString("theme");
         const year = options.getInteger("year");
         if (year) {
-            const action = await themesData.removeThemeData(month, year)
+            const action = await themesData.addThemeData(month, year, theme)
             interaction.reply({content: `${action}`})
         }
         else {
             const year = new Date().getFullYear();
-            const action = await themesData.removeThemeData(month, year)
+            const action = await themesData.addThemeData(month, year, theme)
             interaction.reply({content: `${action}`})
         }
     },
-}
+}   
--- Commands/Moderator/remove_theme.js
+++ Commands/Moderator/remove_theme.js
@@ -4,18 +4,13 @@ const themesData = require("../../data/themesData")
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("add_theme")
+    .setName("remove_theme")
     .setDescription("Allows user to add/change a theme for a month")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .addStringOption(option => 
         option
         .setName("month")
         .setDescription("Choose a month to add/update a theme for")
-        .setRequired(true)
-    )
-    .addStringOption(option => 
-        option
-        .setName("theme")
-        .setDescription("Input a theme for the month")
         .setRequired(true)
     )
     .addIntegerOption(option => 
@@ -28,16 +23,15 @@ module.exports = {
     async execute(interaction) {
         const { channel, options } = interaction;
         const month = options.getString("month");
-        const theme = options.getString("theme");
         const year = options.getInteger("year");
         if (year) {
-            const action = await themesData.addThemeData(month, year, theme)
+            const action = await themesData.removeThemeData(month, year)
             interaction.reply({content: `${action}`})
         }
         else {
             const year = new Date().getFullYear();
-            const action = await themesData.addThemeData(month, year, theme)
+            const action = await themesData.removeThemeData(month, year)
             interaction.reply({content: `${action}`})
         }
     },
-}   
+}
