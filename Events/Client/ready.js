d9bec7f71cbf725bd4dcaff4c0b6945b8a2366ef
sekwanaa <sk123rulz@gmail.com>
2023-05-05 10:42:16 -0400

    setup event handler

---
+++ Events/Client/ready.js
@@ -0,0 +1,10 @@
+const { Client } = require("discord.js");
+
+module.exports = {
+    name: "ready",
+    once: true,
+    execute(client) {
+        console.log(`${client.user.username} is online`);
+        client.user.setActivity(`| .help |`);
+    }
+}
