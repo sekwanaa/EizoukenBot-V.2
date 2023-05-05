e04c0915ceb1dea23e9ca26083357994764e922c
sekwanaa <sk123rulz@gmail.com>
2023-05-05 12:03:58 -0400

    created clear function for moderators

---
+++ Commands/Moderator/clear.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder, CommandInteraction, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Deletes a certain amount of messages in a channel")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addIntegerOption(option => 
+        option
+        .setName("amount")
+        .setDescription("Choose how many messages to delete")
+        .setRequired(true)
+    )
+    .addUserOption(option => 
+        option
+        .setName("target")
+        .setDescription("Choose a specific person to delete messages for")
+        .setRequired(false)
+    ),
+
+    async execute(interaction) {
+        const { channel, options } = interaction;
+        const amount = options.getInteger("amount");
+        const target = options.getUser("target");
+        const messages = await channel.messages.fetch({limit: amount + 1});
+
+        const res = new EmbedBuilder()
+            .setColor(0x5fb041);
+
+        if (target) {
+            let i = 0;
+            const filteredMessages = [];
+
+            (await messages).filter(msg => {
+                if (msg.author.id == target.id && amount > i) {
+                    filteredMessages.push(msg);
+                    i++;
+                }
+            })
+
+            await channel.bulkDelete(filteredMessages).then(messages => {
+                res.setDescription(`Successfully deleted ${messages.size} messages from ${target}`);
+                interaction.reply({embeds: [res]})
+            })
+        }
+        else {
+            await channel.bulkDelete(amount, true).then(messages => {
+                res.setDescription(`Successfully deleted ${messages.size} from the channel`);
+                interaction.reply({embeds: [res]})
+            });
+        }
+    },
+}
--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,9 +1,11 @@
+require("dotenv")
 const { Client } = require("discord.js");
+const mongodb = require("mongodb")
 
 module.exports = {
     name: "ready",
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(`${client.user.username} is online`);
         client.user.setActivity(`| .help |`);
     }
