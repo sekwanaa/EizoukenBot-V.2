595ea5f02800e4ba7b3d34efda98151bac122c48
Sekwang Chia <sk123rulz@gmail.com>
2023-05-08 08:25:07 -0400

    kick function progress

--- Commands/Moderator/kick.js
+++ Commands/Moderator/kick.js
@@ -1,14 +1,36 @@
-const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits } = require("discord.js");
-const themesData = require("../../data/themesData")
+const {
+  SlashCommandBuilder,
+  CommandInteraction,
+  PermissionFlagsBits,
+} = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName("kick")
     .setDescription("Allows user to kick a player")
     .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
-    ,
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("Select who to kick")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("reason")
+        .setDescription("Reason for kick")
+        .setRequired(false)
+    ).addAttachmentOption,
+  async execute(interaction) {
+    const { channel, options } = interaction;
+    const target = options.getUser("target");
+    const reason = options.getString("reason");
 
-    async execute(interaction) {
-        interaction.reply({content: `This command is not ready yet, nice try!`, ephemeral: true})
-    },
-}
+    if (!reason) {
+        const guildMembers = interaction.guild.members.cache.fetch()
+        console.log(guildMembers)
+    }
+
+    // interaction.reply({content: `This command is not ready yet, nice try!`, ephemeral: true})
+  },
+};
