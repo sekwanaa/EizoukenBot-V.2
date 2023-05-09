9bcce4f52e2f14bd7fdf7204d70531286b0b1a39
Sekwang Chia <sk123rulz@gmail.com>
2023-05-09 00:10:30 -0400

    removed unecessary lines in interactionCreate.js

--- Events/Interactions/interactionCreate.js
+++ Events/Interactions/interactionCreate.js
@@ -17,27 +17,7 @@ module.exports = {
 				console.error(error);
             }
         } else if (interaction.isButton()) {
-            console.log("made it to the button")
         } else if (interaction.isStringSelectMenu()) {
-
-            
-
-
-
-
-            // console.log("Made it to string select")
-            // console.log(interaction)
-            // const command = interaction.client.commands.get(interaction.commandName);
-
-            // if (!command) {
-            //     interaction.reply({content: "Outdated command"});
-            // }
-            // try {
-            //     command.execute(interaction, client);
-            // } catch (error) {
-            //     console.error(`Error executing ${interaction.commandName}`);
-			// 	console.error(error);
-            // }
         } else {
             return;
         }
