b58a5e6ad45451245ee74b8c76cb84d71c32e598
Sekwang Chia <sk123rulz@gmail.com>
2023-05-08 22:01:04 -0400

    made volume it's own command, added nowplaying cmd

---
+++ Commands/Music/nowPlaying.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const client = require("../../index");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("nowplaying")
+    .setDescription("Shows information about current song"),
+
+  async execute(interaction) {
+    const { member, guild } = interaction;
+    const voiceChannel = member.voice.channel;
+
+    const embed = new EmbedBuilder();
+
+    if (!voiceChannel) {
+      embed
+        .setColor("Red")
+        .setDescription(
+          "You need to be in a voice channel to execute music commands."
+        );
+      return interaction.reply({ embeds: [embed], ephemeral: true });
+    }
+    if (!member.voice.channel == guild.members.me.voice.channelId) {
+      embed
+        .setColor("Red")
+        .setDescription("I can only be used in one channel at a time.");
+      return interaction.reply({ embeds: [embed] });
+    }
+    const queue = await client.distube.getQueue(voiceChannel);
+    try {
+      if (!queue) {
+        embed.setColor("Red").setDescription("Sorry, there is no queue");
+        return interaction.reply({ embeds: [embed], ephemeral: true });
+      }
+
+      const song = await queue.songs[0];
+
+      embed
+        .setColor("Blue")
+        .setDescription(
+          `📟 | **Currently Playing:** \`${song.name}\` - \`${song.formattedDuration}\` \n**Song Link: **${song.url}`
+        )
+        .setThumbnail(song.thumbnail);
+      return interaction.reply({ embeds: [embed], ephemeral: true });
+    } catch (error) {
+      console.log(error);
+      interaction.reply({
+        content: "sorry something went wrong with your request",
+        ephermeral: true,
+      });
+    }
+  },
+};
--- Commands/Music/volume.js
+++ Commands/Music/volume.js
@@ -3,26 +3,20 @@ const client = require("../../index");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("music")
-    .setDescription("Complete music commands.")
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("volume")
-        .setDescription("Adjust song volume")
-        .addIntegerOption((option) =>
-          option
-            .setName("percent")
-            .setDescription("10 = 10%")
-            .setMinValue(1)
-            .setMaxValue(100)
-            .setRequired(true)
-        )
+    .setName("volume")
+    .setDescription("Adjust song volume")
+    .addIntegerOption((option) =>
+      option
+        .setName("percent")
+        .setDescription("10 = 10%")
+        .setMinValue(1)
+        .setMaxValue(100)
+        .setRequired(true)
     ),
 
   async execute(interaction) {
-    const { options, member, guild, channel } = interaction;
+    const { options, member, guild } = interaction;
     const volume = options.getInteger("percent");
-    const subcommand = options.getSubcommand();
     const voiceChannel = member.voice.channel;
 
     const embed = new EmbedBuilder();
@@ -43,18 +37,15 @@ module.exports = {
     }
     const queue = await client.distube.getQueue(voiceChannel);
     try {
-      switch (subcommand) {
-        case "volume":
-          if (!queue) {
-            embed.setColor("Red").setDescription("Sorry, there is no queue");
-            return interaction.reply({ embeds: [embed], ephemeral: true });
-          }
-          client.distube.setVolume(voiceChannel, volume);
-          return interaction.reply({
-            content: `Volume set to ${volume}%`,
-            ephemeral: true,
-          });
+      if (!queue) {
+        embed.setColor("Red").setDescription("Sorry, there is no queue");
+        return interaction.reply({ embeds: [embed], ephemeral: true });
       }
+      client.distube.setVolume(voiceChannel, volume);
+      return interaction.reply({
+        content: `Volume set to ${volume}%`,
+        ephemeral: true,
+      });
     } catch (error) {
       console.log(error);
       interaction.reply({
