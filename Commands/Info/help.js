480c422a76738409e30506dfa04a9cc590ad8499
Sekwang Chia <sk123rulz@gmail.com>
2023-05-08 23:59:05 -0400

    finally fixed dyunamic help menu

---
+++ Commands/Info/help.js
@@ -0,0 +1,109 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  StringSelectMenuBuilder,
+  ActionRowBuilder,
+  ComponentType
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Collection of all commands"),
+
+  async execute(interaction) {
+    const { channel, options } = interaction;
+    const emojis = {
+      info: "📚",
+      moderator: "🛠️",
+      public: "👪",
+      music: "🎵",
+    };
+
+    const directories = [
+      ...new Set(interaction.client.commands.map((cmd) => cmd.folder)),
+    ];
+
+    const formatString = (str) =>
+      `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+
+    const categories = directories.map((dir) => {
+      const getCommands = interaction.client.commands
+        .filter((cmd) => cmd.folder === dir)
+        .map((cmd) => {
+          return {
+            name: cmd.data.name,
+            description:
+              cmd.data.description ||
+              "There is no description for this command.",
+          };
+        });
+
+      return {
+        directory: formatString(dir),
+        commands: getCommands,
+      };
+    });
+
+    const embed = new EmbedBuilder().setDescription(
+      "Please choose a category from below."
+    );
+
+    const components = (state) => [
+      new ActionRowBuilder().addComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId("help-menu")
+          .setPlaceholder("Please select a category")
+          .setDisabled(state)
+          .addOptions(
+            categories.map((cmd) => {
+              return {
+                label: cmd.directory,
+                value: cmd.directory.toLowerCase(),
+                description: `Commands from ${cmd.directory} category`,
+                emoji: emojis[cmd.directory.toLowerCase() || null],
+              };
+            })
+          )
+      ),
+    ];
+
+    const initialMessage = await interaction.reply({
+      embeds: [embed],
+      components: components(false),
+      ephemeral: true,
+    });
+
+    const filter = (i) => i.user.id === interaction.member.id;
+    
+    const collector = interaction.channel.createMessageComponentCollector({
+        filter,
+        interactionType: 3,
+        time: 30000,
+    });   
+
+    collector.on('collect', async (i) => {
+        const [directory] = i.values;
+        const category = categories.find(
+            (x) => x.directory.toLowerCase() === directory
+        );
+        // await i.reply(`${i.user} has selected ${directory}!`);
+        const categoryEmbed = new EmbedBuilder()
+        .setTitle(`${formatString(directory)} commands`)
+        .setDescription(`A list of all commands under the category ${directory}`)
+        .addFields(
+            category.commands.map((cmd) => {
+                return {
+                    name: `\`${cmd.name}\``,
+                    value: cmd.description,
+                    inline: false,
+                };
+            })
+        );
+        i.update({embeds: [categoryEmbed]})
+    });
+    collector.on('end', () => 
+        initialMessage.edit({components: components(true)})
+    )
+  },
+};
--- Events/Interactions/interactionCreate.js
+++ Events/Interactions/interactionCreate.js
@@ -4,17 +4,44 @@ module.exports = {
     name: "interactionCreate",
 
     execute(interaction, client) {
-        if (!interaction.isChatInputCommand) {
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command) {
+                interaction.reply({content: "Outdated command"});
+            }
+            try {
+                command.execute(interaction, client);
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+            }
+        } else if (interaction.isButton()) {
+            console.log("made it to the button")
+        } else if (interaction.isStringSelectMenu()) {
+
+            
+
+
+
+
+            // console.log("Made it to string select")
+            // console.log(interaction)
+            // const command = interaction.client.commands.get(interaction.commandName);
+
+            // if (!command) {
+            //     interaction.reply({content: "Outdated command"});
+            // }
+            // try {
+            //     command.execute(interaction, client);
+            // } catch (error) {
+            //     console.error(`Error executing ${interaction.commandName}`);
+			// 	console.error(error);
+            // }
+        } else {
             return;
         }
         
-        const command = client.commands.get(interaction.commandName);
-
-        if (!command) {
-            interaction.reply({content: "Outdated command"});
-        }
-        
-        command.execute(interaction, client);
     },
 };
 
