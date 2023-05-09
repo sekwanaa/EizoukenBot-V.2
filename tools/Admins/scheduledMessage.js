0185904665693e929e045f4317581a6fd0c32c18
Sekwang Chia <sk123rulz@gmail.com>
2023-05-09 19:01:28 -0400

    siwtched scheduled message channel to correct one

--- tools/Admins/scheduledMessage.js
+++ tools/Admins/scheduledMessage.js
@@ -3,8 +3,8 @@ const themesData = require("../../data/themesData")
 
 let exportedModules = {
     async scheduledMessage(client) {
-        // const channel = await Client.channels.cache.get("759030440475754497"); //main channel
-        const channel = await client.channels.cache.get("759027798542385164"); //for testing
+        const channel = await client.channels.cache.get("759030440475754497"); //main channel
+        // const channel = await client.channels.cache.get("759027798542385164"); //for testing
         const scheduledMessage = await themesData.scheduledMessage(channel);
     },
 }
