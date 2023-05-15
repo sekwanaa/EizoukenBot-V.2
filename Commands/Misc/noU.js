ed126b2fad0db22142324c440c86bf6ecf9df55d
Sekwang Chia <sk123rulz@gmail.com>
2023-05-15 18:45:58 -0400

    changed the way /nou works - removed selecting a user but rather automatically targets the last person who used the /urmom command

--- Commands/Misc/noU.js
+++ Commands/Misc/noU.js
@@ -3,12 +3,12 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("nou")
-    .setDescription('Mention the user with the message "no u"')
-    .addUserOption((option) => option.setName("target").setDescription("Select a user to mention").setRequired(true)),
+    .setDescription('Mention the most recent "ur mom" user with the message "no u"'),
 
   async execute(interaction) {
-    const { options } = interaction;
-    const target = options.getUser("target")
-    interaction.reply({ content: `${target} no u` });
+    const { channel } = interaction;
+    const urMomMessage = await channel.messages.fetch().then(messages => messages.filter(msg => msg.author.bot && msg.interaction.commandName === 'urmom').first());
+    const previousUser = urMomMessage.interaction.user
+    interaction.reply({ content: `${previousUser} no u` });
   },
 };
