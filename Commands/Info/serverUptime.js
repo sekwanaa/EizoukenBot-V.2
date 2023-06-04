b480e9ce99de13004c5e4eb05cad4f8590c13256
sekwanaa <sk123rulz@gmail.com>
2023-06-04 15:12:34 -0400

    added additional server uptime cmd handling - seconds, minutes, hours, days

--- Commands/Info/serverUptime.js
+++ Commands/Info/serverUptime.js
@@ -7,15 +7,33 @@ module.exports = {
 
   async execute(interaction, client) {
     try {
-      interaction.reply({
-        content: `Uptime: ${Math.round(client.uptime / 1000)} seconds`,
-        ephemeral: true,
-      });
+      if (client.uptime >= 86_400_000) {
+        //more than a day
+        interaction.reply({
+          content: `Uptime: ${Math.round(client.uptime / 86_400_000)} days`,
+          ephemeral: true,
+        });
+      } else if (client.uptime >= 3_600_000) {
+        // more than an hour
+        interaction.reply({
+          content: `Uptime: ${Math.round(client.uptime / 3600000)} hours`,
+          ephemeral: true,
+        });
+      } else if (client.uptime >= 60_000) {
+        // more than a minute
+        interaction.reply({
+          content: `Uptime: ${Math.round(client.uptime / 60000)} minutes`,
+          ephemeral: true,
+        });
+      } else {
+        interaction.reply({
+          // less than a minute
+          content: `Uptime: ${Math.round(client.uptime / 1000)} seconds`,
+          ephemeral: true,
+        });
+      }
     } catch (e) {
-      interaction.reply({
-        content: e,
-        ephemeral: true,
-      });
+      console.log(e);
     }
   },
 };
