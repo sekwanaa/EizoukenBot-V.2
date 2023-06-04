a74f11b0c1b85bb3ebf6c2d7c0f07c6942c1c243
sekwanaa <sk123rulz@gmail.com>
2023-06-04 17:12:35 -0400

    added color and random color cmd - color: shows color preview based on hex - randomColor: generates preview of random hex color

--- Commands/Misc/color.js
+++ Commands/Misc/color.js
@@ -2,11 +2,15 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("randomcolor")
-    .setDescription("Generates a random hex color with a preview"),
+    .setName("color")
+    .setDescription("Generate a color based on a hex code")
+    .addStringOption((option) =>
+      option.setName("hex").setDescription("Hex code").setRequired(true)
+    ),
 
   async execute(interaction) {
-    const color = `#${(Math.random().toString(16) + "000000").substring(2, 8)}`;
+    const { options } = interaction;
+    const color = options.getString("hex");
     const embed = new EmbedBuilder().setDescription(color).setColor(color);
     await interaction.reply({ embeds: [embed], ephemeral: true });
   },
