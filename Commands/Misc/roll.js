1af66074287f7cb209f48edee1b021d2d95bc808
sekwanaa <sk123rulz@gmail.com>
2023-06-04 16:37:47 -0400

    moved serverInfo cmd into info folder - created roll cmd in misc folder

---
+++ Commands/Misc/roll.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("roll")
+    .setDescription("Roll a dice. Default number of sides is 6.")
+    .addIntegerOption((option) =>
+      option.setName("sides").setDescription("The number of sides").setRequired(false)
+    ),
+
+  async execute(interaction) {
+    const { options } = interaction;
+    const sides = options.getInteger("sides") || 6;
+
+    const roll = Math.floor(Math.random() * sides) + 1;
+    await interaction.reply({
+      content: `${roll}`,
+      ephemeral: true,
+    });
+  },
+};
