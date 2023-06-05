fb675cb3b4865e9cb0744f45b464c18c5385ca28
sekwanaa <sk123rulz@gmail.com>
2023-06-04 22:30:28 -0400

    removed ephemeral from avatar command

--- Commands/Misc/getAvatar.js
+++ Commands/Misc/getAvatar.js
@@ -1,20 +1,20 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("avatar")
-    .setDescription("Get the avatar of yourself or another user")
-    .addUserOption((option) =>
-      option
-        .setName("user")
-        .setDescription("The user you want to get the avatar of")
-        .setRequired(false)
-    ),
+	data: new SlashCommandBuilder()
+		.setName('avatar')
+		.setDescription('Get the avatar of yourself or another user')
+		.addUserOption(option =>
+			option
+				.setName('user')
+				.setDescription('The user you want to get the avatar of')
+				.setRequired(false)
+		),
 
-  async execute(interaction) {
-    const { options } = interaction;
-    const user = options.getUser("user") || interaction.user;
-    const embed = new EmbedBuilder().setImage(user.displayAvatarURL({ dynamic: true }));
-    await interaction.reply({ embeds: [embed], ephemeral: true });
-  },
-};
+	async execute(interaction) {
+		const { options } = interaction
+		const user = options.getUser('user') || interaction.user
+		const embed = new EmbedBuilder().setImage(user.displayAvatarURL({ dynamic: true }))
+		await interaction.reply({ embeds: [embed] })
+	},
+}
