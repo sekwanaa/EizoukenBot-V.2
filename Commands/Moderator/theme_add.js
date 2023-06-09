98de82411bbabdb3d3df03ab7bb0a47bdc702b23
Sekwang Chia <sk123rulz@gmail.com>
2023-06-08 23:54:28 -0400

    added set choices for when adding/removing a theme - reduces potential mistakes when inputting months

--- Commands/Moderator/theme_add.js
+++ Commands/Moderator/theme_add.js
@@ -10,6 +10,20 @@ module.exports = {
 			option
 				.setName('month')
 				.setDescription('Choose a month to add/update a theme for')
+				.setChoices(
+					{ name: 'January', value: 'january' },
+					{ name: 'February', value: 'february' },
+					{ name: 'March', value: 'march' },
+					{ name: 'April', value: 'april' },
+					{ name: 'May', value: 'may' },
+					{ name: 'June', value: 'june' },
+					{ name: 'July', value: 'july' },
+					{ name: 'August', value: 'august' },
+					{ name: 'September', value: 'september' },
+					{ name: 'October', value: 'october' },
+					{ name: 'November', value: 'november' },
+					{ name: 'December', value: 'december' }
+				)
 				.setRequired(true)
 		)
 		.addStringOption(option =>
--- Commands/Moderator/theme_remove.js
+++ Commands/Moderator/theme_remove.js
@@ -9,6 +9,20 @@ module.exports = {
 		.addStringOption(option =>
 			option
 				.setName('month')
+				.setChoices(
+					{ name: 'January', value: 'january' },
+					{ name: 'February', value: 'february' },
+					{ name: 'March', value: 'march' },
+					{ name: 'April', value: 'april' },
+					{ name: 'May', value: 'may' },
+					{ name: 'June', value: 'june' },
+					{ name: 'July', value: 'july' },
+					{ name: 'August', value: 'august' },
+					{ name: 'September', value: 'september' },
+					{ name: 'October', value: 'october' },
+					{ name: 'November', value: 'november' },
+					{ name: 'December', value: 'december' }
+				)
 				.setDescription('Choose a month to add/update a theme for')
 				.setRequired(true)
 		)
