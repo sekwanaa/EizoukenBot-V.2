65a23c834c7747ad838b4f63a61a46c1c186567b
Sekwang Chia <sk123rulz@gmail.com>
2024-02-17 03:55:56 -0500

    added shuffle command

---
+++ Commands/Music/shuffle.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const client = require('../../index')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('loop')
+		.setDescription('Loop current song or current queue'),
+
+	async execute(client, interaction) {
+		try {
+			const queue = client.player.getQueue(interaction.guildId)
+
+			if (!queue) {
+				return await interaction.editReply('There are no songs in the queue.')
+			}
+
+			queue.shuffle()
+			await interaction.reply({
+				content: 'The queue has been successfully shuffled!',
+				ephemeral: true,
+			})
+		} catch (error) {
+			console.log(error)
+			interaction.reply({
+				content: 'sorry something went wrong with your request',
+				ephermeral: true,
+			})
+		}
+	},
+}
